fe38b3c6611678634b2d837b6487539ce86e5c2b
vaishkash <137786788+omAtDV@users.noreply.github.com>
2024-11-26 18:24:34 +0530

    remove react from react import

--- src/pages/Kids.tsx
+++ src/pages/Kids.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { Link } from "react-router-dom";
 // import Navbar from '../Navbar'
 import profile from "../assets/profile.png";
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import Login from '../components/Login'
 
 function Profile() {
